59192df96b2e7831086381743815f92e5b35b9f4
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-04-01 15:16:28 +0700

    Add files via upload

---
+++ TestMyLinkedList.java
@@ -0,0 +1,12 @@
+public class TestMyLinkedList {
+    public  static void main(String[] args){
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MylinkedList ll = new MylinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+        ll.add(4,9);
+        ll.add(4,8);
+        ll.printList();
+    }
+}
